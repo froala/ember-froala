c49b9b3e0c8cf9fe8a93ecdeba0ed660e6929754
Maulik Bengali <maulik.bengali@gmail.com>
2015-08-24 18:46:32 +0530

    Added component logic

--- addon/components/froala-editor.js
+++ addon/components/froala-editor.js
@@ -2,5 +2,58 @@ import Ember from 'ember';
 import layout from '../templates/components/froala-editor';
 
 export default Ember.Component.extend({
-  layout: layout
+    layout: layout,
+
+    tagName: 'div',
+    _froala: null,
+    inlineMode: false,
+    placeholder: 'Enter',
+    value: null,
+    allowHTML: true,
+    allowBlank: true,
+
+    getConfig: function() {
+        var appConfig = this.container.lookup('config:environment');
+        return appConfig && appConfig.get('froala-editor') || {};
+    },
+
+    didInsertElement: function() {
+        var froala = this.$().editable({
+                inlineMode: this.get('inlineMode'),
+                placeholder: this.get('placeholder'),
+                value: this.get('value'),
+                buttons:[],
+                imageUploadToS3: this.getConfig().imageUploadToS3
+            });
+
+        this.$().editable('setHTML', this.get('value') || '', false);
+
+        this.$().on('editable.blur', Ember.$.proxy(this.onFocusOut, this));
+
+        this.set('_froala', froala);
+    },
+
+    onFocusOut: function() {
+        if (!this.get('allowBlank')) {
+            var text = this.get('allowHTML') ? this.$().editable('getHTML', true, true) : this.$().editable('getText');
+
+            if (text && text.trim().length > 0) {
+                this.set('value', text); 
+            } else {
+                this.$().editable('setHTML', this.get('value'));
+            }
+        } else {
+            if (this.get('allowHTML')) {
+                this.set('value', this.$().editable('getHTML', true, true)); 
+            } else {
+                this.set('value', this.$().editable('getText')); 
+            }
+        }
+    },
+
+    willDestroyElement: function() {
+        if (this.get('_froala')) {
+            this.$().editable('destroy');
+        }
+    }
 });
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/FroalaWysiwygEditor/css/froala.style.min.css');
-    app.import(app.bowerDirectory + '/FroalaWysiwygEditor/js/froala.editor.min.js');
+    app.import(app.bowerDirectory + '/FroalaWysiwygEditor/css/froala_style.min.css');
+    app.import(app.bowerDirectory + '/FroalaWysiwygEditor/js/froala_editor.min.js');
   }
 };

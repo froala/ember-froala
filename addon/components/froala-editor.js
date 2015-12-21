882a43a4ca82844ccd30cd03f068b2498f455b2c
Mark Campbell <artsmc88@gmail.com>
2015-12-21 07:35:49 -0500

    Update froala-editor.js
    simplify the handleEvent to include the key.

--- addon/components/froala-editor.js
+++ addon/components/froala-editor.js
@@ -18,20 +18,22 @@ export default Ember.Component.extend({
         for(var prop in this.attrs){
           if(prop !='params') {
             var key = prop.replace(/_/g,".");           
-            froalaElement.on('froalaEditor.' + key, proxy(this.handleFroalaEvent, this));
+            froalaElement.on('froalaEditor.' + key, proxy(this.handleFroalaEvent, this, key));
           }
         }
         this.set('_froala', froala);
     },
-
-    handleFroalaEvent: function(event, editor) {
-      var events = this.get('eventNames');
+    handleFroalaEvent: function(key,event, editor,x,y,z) {
       const eventName = event.namespace;
-      const reverseEventName = eventName.replace(/\./g,"_");
+      const reverseEventName = key.replace(/\./g,"_");
+      const actionHandler = this.attrs[reverseEventName];
       if(isFunction(actionHandler)) {
         actionHandler(event, editor);
       } else {
-        this.sendAction(reverseEventName, event, editor);
+        var action =  this.attrs[reverseEventName];
+        if(action===false){
+          return false;
+        }
       }
     },
     willDestroyElement: function() {
